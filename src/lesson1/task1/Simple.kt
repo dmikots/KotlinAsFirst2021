5453509ca57ee9b8714a933f16fd0cf58c866ced
dmikots <mikotsd@gmail.com>
2023-08-14 21:08:14 +0300

    1я задача

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -65,7 +65,8 @@ fun main() {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int =
+    ((hours * 3600) + (minutes * 60) + seconds)
 
 /**
  * Тривиальная (1 балл)
@@ -75,6 +76,7 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
 fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
+//    ((((sagenes * 3 * 48) + (arshins * 48) + vershoks)) / 100)
 
 /**
  * Тривиальная (1 балл)
